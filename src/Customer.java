83faab8a6b4807d95feb649d6487c668c929c86d
lizjones97 <101881376+lizjones97@users.noreply.github.com>
2022-03-28 21:11:43 +0100

    Update Customer.java
    revert back to your hierachies chart for the first coursework.

--- src/Customer.java
+++ src/Customer.java
@@ -4,6 +4,15 @@
  * @author Yusuf Dauda
  * @version 1.0
  */
+import javafx.fxml.FXML;
+import java.util.objects;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import java.io.IOException;
+
 public class Customer {
 	private String firstName;
 	private String lastName;
