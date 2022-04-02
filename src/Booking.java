fe74e98f27c787437133e3989250af5b44abae89
Yusuf Dauda <yusuf_dd@yahoo.com>
2022-04-02 17:10:28 +0100

    undo liz changes3

--- src/Booking.java
+++ src/Booking.java
@@ -4,16 +4,6 @@
  * @author Megan Symons
  * @version 1.0
  */
-import javafx.fxml.FXML;
-import java.util.objects;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import java.io.IOException;
-
-
 public class Booking {
 	private int numOfGuests;
 	private int custId;
